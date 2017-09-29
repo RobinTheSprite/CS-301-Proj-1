caa5b24facfbd5662c608f8c0adb3b38fccd221a
RobinTheSprite <madscientistak@gmail.com>
2017-09-28 19:40:45 -0800

    I can print a character from a file!

--- iofunctions.asm
+++ iofunctions.asm
@@ -4,19 +4,25 @@ global bar
 bar:
 	extern fopen
 	extern fclose
-	extern puts
+	extern fgetc
+	extern putchar
 
-	mov rcx, string
-	call puts
-	;call fopen
-	;mov rcx, rax
-	;call fclose
+	sub rsp,40
+	call fopen
+	mov rcx, rax
+	add rsp,40
+	push rcx
 
+	sub rsp,40
+	call fgetc
+	mov rcx,rax
+	call putchar
+	add rsp,40
+
+	pop rcx
+	sub rsp,40
+	call fclose
+	add rsp,40
+	
 	mov rax,0
 	ret
-
-
-section .data
-
-string:
-	db "test.txt",0
--- iomain.cpp
+++ iomain.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
-extern "C" int bar();
+extern "C" int bar(const char * filename, const char * mode);
 
 int main()
 {
-	bar();
+	bar("test.txt", "r");
 
 	while (std::cin.get() != '\n')
 
