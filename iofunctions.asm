16fcd1ef044b9ffb4c6437fc5dd04fa3e3a1071f
RobinTheSprite <madscientistak@gmail.com>
2017-10-03 11:10:09 -0800

    Can print a string now

--- iofunctions.asm
+++ iofunctions.asm
@@ -1,28 +1,61 @@
+; iofunctions.asm
+; Mark Underwood
+; 9.30.17
+; Contains functions designed to be run in iomain.cpp
+
 section .text
-global bar
 
-bar:
-	extern fopen
-	extern fclose
-	extern fgetc
-	extern putchar
+;------ Getting everything in the right scope ---------------;
+global fopenASM
+global fcloseASM
+global hideStrings
+global printfASM
 
+extern fopen
+extern fclose
+extern fscanf
+extern puts
+;------------------------------------------------------------;
+
+;------ Functions -------------------------------------------;
+fopenASM:
 	sub rsp,40
 	call fopen
-	mov rcx, rax
 	add rsp,40
-	push rcx
-
+	ret
+	
+printfASM:
 	sub rsp,40
-	call fgetc
-	mov rcx,rax
-	call putchar
+	call puts
 	add rsp,40
+	ret
 
-	pop rcx
+fcloseASM:	
 	sub rsp,40
 	call fclose
 	add rsp,40
-	
-	mov rax,0
 	ret
+
+hideStrings:
+	mov rdx, inputForFscanf
+	mov r8,readString
+
+	sub rsp,40
+	call fscanf
+	add rsp,40
+
+	mov rcx,readString
+
+	sub rsp,40
+	call printfASM
+	add rsp,40
+	
+	ret
+
+;------------------------------------------------------------;
+
+section .data
+readString:
+	db '',0
+inputForFscanf:
+	db '%s',0
--- iomain.cpp
+++ iomain.cpp
@@ -1,12 +1,35 @@
+/*
+iomain.cpp
+Mark Underwood
+10.2.17
+Uses some assembly language to play with text files
+*/
 
 #include <iostream>
-#include <stdio.h>
-#include <string.h>
-extern "C" int bar(const char * filename, const char * mode);
+
+
+extern "C" FILE * fopenASM(const char * filename, const char * mode);
+extern "C" void fcloseASM(FILE * fptr);
+extern "C" void hideStrings(FILE * fptr);
+extern "C" void printfASM(char * string);
+
+void microsoftWhy()
+{
+	FILE * nonsense = nullptr;
+	int moreNonsense = 0;
+	#pragma warning(suppress : 4996)
+	fscanf(nonsense, "%i", &moreNonsense);
+}
 
 int main()
 {
-	bar("test.txt", "r");
+	FILE * fptr = fopenASM("test.txt", "r");
+
+	hideStrings(fptr);
+	
+	//printfASM("Hallo");
+
+	fcloseASM(fptr);
 
 	while (std::cin.get() != '\n')
 
